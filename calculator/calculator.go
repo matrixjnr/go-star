a9cad31b4ba28d6e8804e2aa17eba0a0e514e989
John Simiyu <jonnyeinstein@gmail.com>
2024-07-24 23:09:31 +0300

    messed up calculator

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -4,24 +4,6 @@ import (
 	"fmt"
 )
 
-// a calculator that follows bodmas
-func Calculate(a int, b int, operator string) int {
-	switch operator {
-	case "+":
-		return a + b
-	case "-":
-		return a - b
-	case "*":
-		return a * b
-	case "/":
-		return a / b
-	default:
-		fmt.Println("Invalid Operator")
-		return 0
-	}
-}
-
-// a calculator that follows bodmas
 func CalculateFloat(a float64, b float64, operator string) float64 {
 	switch operator {
 	case "+":
@@ -38,26 +20,9 @@ func CalculateFloat(a float64, b float64, operator string) float64 {
 	}
 }
 
-// a calculator that follows bodmas
-func CalculateIntegers(a int, b int, operator string) int {
-	switch operator {
-	case "+":
-		return a + b
-	case "-":
-		return a - b
-	case "*":
-		return a * b
-	case "/":
-		return a / b
-	default:
-		fmt.Println("Invalid Operator")
-		return 0
-	}
-}
-
 func main() {
-	fmt.Println(Calculate(1, 2, "+"))
-	fmt.Println(Calculate(1, 2, "-"))
-	fmt.Println(Calculate(1, 2, "*"))
+	fmt.Println(CalculateFloat(1, 2, "+"))
+	fmt.Println(CalculateFloat(1, 2, "-"))
+	fmt.Println(CalculateFloat(1, 2, "*"))
 	fmt.Println(CalculateFloat(1, 2, "/"))
 }
